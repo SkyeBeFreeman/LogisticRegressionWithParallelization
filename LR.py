49863893be020f120f787b01bb618bd901a613ec
Skye <928016560@qq.com>
2017-06-06 17:22:54 +0800

    update data reading method

--- LR.py
+++ LR.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 import datetime,time
 
-# 数据列数 = 1 + 133
+# 数据列数 = 1 + 132
 
 train_x = []
 train_y = []
@@ -17,21 +17,24 @@ print(time.strftime("%Y-%m-%d %H:%M:%S") + " 训练集读入中...", flush=True)
 with open("train_data.txt", 'r') as file:
     cnt = 0
     for line in file:
-        train_row_data = [0] * 133
+        train_row_data = [0] * 132
         row_data = str(line).split()
         train_y.append(int(row_data[0]))
         for i in range(1,len(row_data)):
             key_value = row_data[i].split(":")
             key = int(key_value[0])
             value = float(key_value[1])
-            if (key <= 132):
-                train_row_data[key] = value
+            if (key <= 131):
+                train_row_data[key - 1] = value
         train_x.append(train_row_data)
         cnt += 1
         # if (cnt == 500):
         #     break
 train_x = np.array(train_x)
-train_y = np.array(train_y)
+train_y = np.array(train_y).reshape((-1,1))
+print("train_x type: " + str(type(train_x)))
+print("train_x shape: " + str(train_x.shape))
+print("train_y type: " + str(type(train_y)))
+print("train_y shape: " + str(train_y.shape))
 print(time.strftime("%Y-%m-%d %H:%M:%S") + " 训练集读取完成", flush=True)
-print(train_x)
-print(train_y)
+
