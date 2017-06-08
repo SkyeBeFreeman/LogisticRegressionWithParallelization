e1bd6197baf348b20dd10ec5032a43ffd5c92f83
Skye <928016560@qq.com>
2017-06-08 23:54:35 +0800

    完成单线程LR算法

--- LR.py
+++ LR.py
@@ -1,9 +1,8 @@
 # coding: utf-8
 
-from random import seed
-from random import randrange
+from random import seed, randrange, random
 from math import log, exp, pow, fabs
-import datetime,time
+from time import strftime
 
 # 数据列数 = 1 + 132
 
@@ -19,7 +18,7 @@ train_y = []
 m = 0
 n = 132
 # 加载训练集
-print(time.strftime("%Y-%m-%d %H:%M:%S") + " 训练集读入中...", flush=True)
+print(strftime("%Y-%m-%d %H:%M:%S") + " 训练集读入中...", flush=True)
 with open("train_data.txt", 'r') as file:
     for line in file:
         train_row_data = [0] * 132
@@ -38,9 +37,32 @@ with open("train_data.txt", 'r') as file:
 
 print("train_x type: " + str(type(train_x)))
 print("train_y type: " + str(type(train_y)))
-print(time.strftime("%Y-%m-%d %H:%M:%S") + " 训练集读取完成", flush=True)
+print(strftime("%Y-%m-%d %H:%M:%S") + " 训练集读取完成", flush=True)
 
-# 平均归一化训练集feature
+# 读取测试集
+print(strftime("%Y-%m-%d %H:%M:%S") + " 测试集读入中...", flush=True)
+test_m = 0
+test_x = []
+with open("test_data.txt", 'r') as file:
+    test_id = set()
+    for line in file:
+        test_row_data = [0] * 132
+        row_data = str(line).split()
+        id = int(row_data[0])
+        if not (id in test_id):
+            test_id.add(id)
+            for i in range(1,len(row_data)):
+                key_value = row_data[i].split(":")
+                key = int(key_value[0])
+                value = float(key_value[1])
+                test_row_data[key - 1] = value
+            test_x.append(test_row_data)
+            test_m += 1
+
+print(test_m, flush=True)
+print(strftime("%Y-%m-%d %H:%M:%S") + " 测试集读取完成", flush=True)
+
+# 平均归一化训练集和测试集的feature
 for i in range(n):
     x_min = train_x[0][i]
     x_max = train_x[0][i]
@@ -54,19 +76,33 @@ for i in range(n):
         x_diff = 1
     for j in range(m):
         train_x[j][i] = (train_x[j][i] - x_min) * 1.0 / x_diff
+    for j in range(test_m):
+        test_x[j][i] = (test_x[j][i] - x_min) * 1.0 / x_diff
+
+# print(test_x[:20][:], flush=True)
 
 # 一些参数的初始化
 alpha = 0.1
 threshold = 0.000001
-lmd = 0.1
+lmd = 0
 step = 1
-theta = [0.5] * n
+# theta = [0.5] * n
+theta = [random() for i in range(n)]
 
 # sigmoid函数
 def sigmoid(X, m):
     result = X
     for i in range(m):
-        result[i][0] = 1 / (1 + exp(-X[i][0]))
+        if X[i][0] > 100:
+            result[i][0] = 1
+        if X[i][0] < -100:
+            result[i][0] = 0
+        try:
+            result[i][0] = 1 / (1 + exp(-X[i][0]))
+        except Exception as e:
+            print (e)
+            print(X[i][0])
+            print(test_x[i])
     return result
 
 # 矩阵点乘
@@ -111,11 +147,12 @@ def getCost(m, n, lmd, h, theta):
 
 
 # 训练模型
-print(time.strftime("%Y-%m-%d %H:%M:%S") + " 开始训练", flush=True)
+print(strftime("%Y-%m-%d %H:%M:%S") + " 开始训练", flush=True)
 cost = 0
 change = 1
 cnt = 0
-while (change >= threshold):
+# while (change >= threshold):
+for x in range(1000):
     h = sigmoid(dotMultiply(train_x, T(theta), m, n), m)
     # print(h)
     new_cost = getCost(m, n, lmd, h, theta)
@@ -130,5 +167,29 @@ while (change >= threshold):
     cost = new_cost
     cnt += 1
     if (cnt % step == 0):
-        print('cost:', cost)
-print(time.strftime("%Y-%m-%d %H:%M:%S") + " 训练结束", flush=True)
+        print('cost:', cost, flush=True)
+print(strftime("%Y-%m-%d %H:%M:%S") + " 训练结束", flush=True)
+
+# print(theta)
+# mymax = 0
+# for i in range(n):
+#     if mymax < fabs(theta[i]):
+#         mymax = fabs(theta[i])
+# print(mymax)
+
+print(strftime("%Y-%m-%d %H:%M:%S") + " 开始预测", flush=True)
+h = sigmoid(dotMultiply(test_x, T(theta), test_m, n), test_m)
+# print(h)
+print(strftime("%Y-%m-%d %H:%M:%S") + " 预测结束", flush=True)
+
+rounded = [round(x) for x in h]
+
+with open('result.txt', 'w') as output:
+    index = 0
+    output.write('id,label\n')
+    for i in rounded:
+        Id, val = index, i
+        output.write('{},{}\n'.format(Id, val))
+        index += 1
+
+print(strftime("%Y-%m-%d %H:%M:%S") + " 结束", flush=True)
