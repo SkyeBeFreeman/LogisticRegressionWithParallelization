161a06ee6ef8b1bf970b4f4f680ea0c774a25f1f
Skye <928016560@qq.com>
2017-06-08 12:54:17 +0800

    完成训练模型

--- LR.py
+++ LR.py
@@ -2,7 +2,7 @@
 
 from random import seed
 from random import randrange
-from math import sqrt, log, exp, pow
+from math import log, exp, pow, fabs
 import datetime,time
 
 # 数据列数 = 1 + 132
@@ -21,7 +21,6 @@ n = 132
 # 加载训练集
 print(time.strftime("%Y-%m-%d %H:%M:%S") + " 训练集读入中...", flush=True)
 with open("train_data.txt", 'r') as file:
-    global m
     for line in file:
         train_row_data = [0] * 132
         row_data = str(line).split()
@@ -34,13 +33,11 @@ with open("train_data.txt", 'r') as file:
                 train_row_data[key - 1] = value
         train_x.append(train_row_data)
         m += 1
-        # if (m == 500):
-        #     break
+        if (m == 100):
+            break
 
 print("train_x type: " + str(type(train_x)))
-print("train_x shape: " + str(train_x.shape))
 print("train_y type: " + str(type(train_y)))
-print("train_y shape: " + str(train_y.shape))
 print(time.strftime("%Y-%m-%d %H:%M:%S") + " 训练集读取完成", flush=True)
 
 # 平均归一化训练集feature
@@ -53,31 +50,31 @@ for i in range(n):
         if (x_max < train_x[j][i]):
             x_max = train_x[j][i]
     x_diff = (x_max - x_min) * 1.0
+    if (x_diff == 0):
+        x_diff = 1
     for j in range(m):
         train_x[j][i] = (train_x[j][i] - x_min) * 1.0 / x_diff
 
 # 一些参数的初始化
 alpha = 0.1
-threshold = 0.0000001
+threshold = 0.000001
 lmd = 0.1
 step = 1
-theta = [1] * n
+theta = [0.5] * n
 
 # sigmoid函数
-def sigmoid(X):
-    ans = 0
-    for i in X:
-        ans += 1 / (1 + exp(-i))
-    return ans
+def sigmoid(X, m):
+    result = X
+    for i in range(m):
+        result[i][0] = 1 / (1 + exp(-X[i][0]))
+    return result
 
 # 矩阵点乘
-def dotMultiply(X, Y):
-    X_row = X.shape[0]
-    X_col = X.shape[1]
-    Y_row = Y.shape[0]
-    Y_col = Y.shape[1]
-    if X_col != Y_row:
-        raise MyError("矩阵大小不适配")
+def dotMultiply(X, Y, m, n):
+    X_row = m
+    X_col = n
+    Y_row = n
+    Y_col = 1
     result = []
     for i in range(X_row):
         line = []
@@ -86,22 +83,6 @@ def dotMultiply(X, Y):
             for a in range(X_col):
                 ans += X[i][a] * Y[a][j]
             line.append(ans)
-        result.append(line)
-    return result
-
-# 矩阵对应位置相乘
-def myMultiply(X, Y):
-    X_row = X.shape[0]
-    X_col = X.shape[1]
-    Y_row = Y.shape[0]
-    Y_col = Y.shape[1]
-    if X_col != Y_col or X_row != Y_row:
-        raise MyError("矩阵大小不匹配")
-    result = []
-    for i in range(X_row):
-        line = []
-        for j in range(Y_col):
-            line.append(X[i][j] * Y[i][j])
         result.append(line)
     return result
 
@@ -114,33 +95,40 @@ def T(X):
         result.append(line)
     return result
 
-
-# 矩阵第一列求和
-def mySum(X):
-    X_row = X.shape[0]
-    ans = 0
-    for i in range(X_row):
-        ans += X[i][0]
-    return ans
-
 # 获取代价函数值
 def getCost(m, n, lmd, h, theta):
     ans = 0
     for i in range(m):
-        if train_y[i][0] == 1:
-            ans += math.log(h[i][0]) * (-1.0) / m
+        if train_y[i] == 1:
+            ans += log(h[i][0]) * (-1.0) / m
         else:
-            ans += math.log(1.0 - h[i][0]) * (-1.0) / m
+            ans += log(1.0 - h[i][0]) * (-1.0) / m
     regularzilation = 0
     for i in range(n):
         regularzilation += pow(theta[i], 2)
     regularzilation *= lmd / (2 * m)
     return ans + regularzilation
 
+
 # 训练模型
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " 开始训练", flush=True)
 cost = 0
 change = 1
 cnt = 0
 while (change >= threshold):
-    h = sigmoid(dotMultiply(train_x, T(theta)))
+    h = sigmoid(dotMultiply(train_x, T(theta), m, n), m)
+    # print(h)
     new_cost = getCost(m, n, lmd, h, theta)
+    for i in range(n):
+        gradient = 0
+        for j in range(m):
+            gradient += (h[j][0] - train_y[j]) * train_x[j][i]
+        gradient -= lmd * theta[i]
+        gradient *= ((- alpha) / m)
+        theta[i] += gradient
+    change = fabs(cost - new_cost)
+    cost = new_cost
+    cnt += 1
+    if (cnt % step == 0):
+        print('cost:', cost)
+print(time.strftime("%Y-%m-%d %H:%M:%S") + " 训练结束", flush=True)
